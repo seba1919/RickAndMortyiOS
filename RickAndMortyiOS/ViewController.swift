ea57b3faefa6467a588cde1acdfbed0adee2b119
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmaill.com>
2020-08-14 18:03:40 +0200

    Initial Commit

---
+++ RickAndMortyiOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RickAndMortyiOS
+//
+//  Created by Sebastian Wiatrzyk on 14/08/2020.
+//  Copyright © 2020 Sebastian Wiatrzyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
