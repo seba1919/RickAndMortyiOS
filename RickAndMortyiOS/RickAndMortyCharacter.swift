0b8814cc6dd041cda164158cfcffe751852665ab
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmaill.com>
2020-08-14 18:05:21 +0200

    add RickAndMortyCharacter model object

---
+++ RickAndMortyiOS/RickAndMortyCharacter.swift
@@ -0,0 +1,18 @@
+//
+//  RickAndMortyCharacter.swift
+//  RickAndMortyiOS
+//
+//  Created by Sebastian Wiatrzyk on 14/08/2020.
+//  Copyright © 2020 Sebastian Wiatrzyk. All rights reserved.
+//
+
+import Foundation
+
+struct RickAndMortyCharacter {
+    public let id: UUID
+    public let name: String
+    public let imageURL: URL
+    public let gender: String
+    public let status: String
+    public let origin: String
+}
