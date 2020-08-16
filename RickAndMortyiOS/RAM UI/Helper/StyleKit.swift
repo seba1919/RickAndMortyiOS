3d6a22c44f3d1cf7495bda4498f91a8f2ad1c2c4
Sebastian Wiatrzyk <sebastian.wiatrzyk@gmaill.com>
2020-08-16 16:42:46 +0200

    add StyleKit and UIColor+hex helpers

---
+++ RickAndMortyiOS/RAM UI/Helper/StyleKit.swift
@@ -0,0 +1,14 @@
+//
+//  StyleKit.swift
+//  RickAndMortyiOS
+//
+//  Created by Sebastian Wiatrzyk on 16/08/2020.
+//  Copyright © 2020 Sebastian Wiatrzyk. All rights reserved.
+//
+
+import UIKit
+
+struct StyleKit {
+    static let mainColor = UIColor(rgb: 0x212328)
+    static let secondaryColor = UIColor(rgb: 0x3c3e43)
+}
